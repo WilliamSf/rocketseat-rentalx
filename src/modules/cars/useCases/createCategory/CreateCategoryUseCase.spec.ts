680aefe73f64c2057c63b302ceac40079e34080a
William Silva <william.faria@rarolabs.com.br>
2023-01-11 17:11:06 -0300

    Inicio do chapter IV, aula: Teste de criação de categoria

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,45 @@
+import { AppError } from "../../../../errors/AppError";
+import CategoriesRepositoryInMemory from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe('Create Category', () => {
+    let createCategoryUseCase: CreateCategoryUseCase;
+    let categoriesRepositoryInMemory:CategoriesRepositoryInMemory;
+    
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+    });
+
+    it('should be able to create new category', async () => {
+        const newCategory = {
+            name: 'Category Test',
+            description: 'Category description Test'
+        };
+
+        await createCategoryUseCase.execute(newCategory);
+        
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(
+            newCategory.name
+        );
+
+        expect(categoryCreated).toHaveProperty('id');
+    });
+
+    it('should not be able to create new category with name exists', async () => {
+        expect(async () => {
+            const newCategory = {
+                name: 'Category Test',
+                description: 'Category description Test'
+            };
+    
+            await createCategoryUseCase.execute(
+                newCategory
+            );
+            
+            await createCategoryUseCase.execute(
+                newCategory
+            );
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
