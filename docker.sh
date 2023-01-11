bcbae89bfa1b08fd131c1f392dc8b98351c522bc
William Silva <william.faria@rarolabs.com.br>
2023-01-11 14:27:55 -0300

    adiciona bash docker

---
+++ docker.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+show_menu() {
+    local INPUT=/tmp/input.$$
+    dialog --title "Docker" \
+        --ok-label "Select" \
+        --cancel-label "Exit" \
+        --menu "Choose one of the following options:" 15 60 8 \
+        1 "Start Containers" \
+        2 "Start Containers (detached mode)" \
+        3 "Build and Start Containers" \
+        4 "Stop Containers" \
+        5 "Reset Docker" \
+        2>$INPUT
+
+    menuitem=$(<"$INPUT")
+
+    rm $INPUT 2>/dev/null
+    sleep 0.1
+    clear
+
+    case $menuitem in
+    1) docker_start ;;
+    2) docker_start --detach ;;
+    3) docker_build ;;
+    4) docker_stop ;;
+    5) docker_reset ;;
+    esac
+}
+
+docker_start() {
+    echo -e "\nStarting Docker Compose...\n"
+    sudo docker-compose up "$@"
+}
+
+docker_build() {
+    echo -e "\nBuilding Docker Compose...\n"
+    sudo docker-compose up --build --no-start
+    wait
+}
+
+docker_stop() {
+    echo -e "\nStopping Docker Compose...\n"
+    sudo docker-compose down
+}
+
+docker_reset() {
+
+    clear
+    dialog --title "Reset Docker" \
+        --yesno "Are you sure you want to reset Docker?" 7 60
+    if [ $? -eq 0 ]; then
+        clear
+        sleep 0.1
+
+        sudo docker stop $(sudo docker ps -a -q) 2>/dev/null
+        sudo docker rm $(sudo docker ps -a -q) 2>/dev/null
+
+        sudo docker volume rm $(sudo docker volume ls -qf dangling=true) 2>/dev/null
+        sudo docker image rm $(sudo docker images -q) 2>/dev/null
+        sudo docker rmi $(sudo docker images -q) 2>/dev/null
+
+        # yes | sudo docker system prune -a 2>/dev/null
+        yes | sudo docker volume prune -a 2>/dev/null
+        yes | sudo docker network prune -a 2>/dev/null
+        yes | sudo docker images prune -a 2>/dev/null
+
+        clear
+
+        echo -e "\nDocker has been reset.\n"
+    fi
+}
+
+clear
+sleep 0.1
+show_menu
+
+# docker_reset
+# docker_compose_reset
+
+# sudo docker-compose up --build
+
+# sudo docker ps -a
