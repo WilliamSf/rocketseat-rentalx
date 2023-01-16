d09c7ee668f95275bf029032b18fc30bb192e45f
William Silva <william.faria@rarolabs.com.br>
2023-01-16 15:34:19 -0300

    Aula: Criando middleware de administrador

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,5 +1,7 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { Router } from "express";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
@@ -7,6 +9,8 @@ const createCarController = new CreateCarController();
 
 carsRoutes.post(
   "/",
+  ensureAuthenticated,
+  ensureAdmin,
   createCarController.handle
 );
 
--- src/shared/infra/http/routes/specifcations.routes.ts
+++ src/shared/infra/http/routes/specifcations.routes.ts
@@ -1,12 +1,16 @@
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 import { Router } from "express";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const specifcationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specifcationsRoutes.use(ensureAuthenticated);
-specifcationsRoutes.post('/', createSpecificationController.handle);
+specifcationsRoutes.post('/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createSpecificationController.handle
+);
 
 export { specifcationsRoutes };
